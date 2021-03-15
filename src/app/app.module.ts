7923151f393514baaf724c3a6893de5d868e0e9e
Janos <romanjanos0504@gmail.com>
2021-03-15 09:25:34 +0100

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductListComponent } from './common/product-list/product-list.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductListComponent,
+    NavigationComponent,
+    
   ],
   imports: [
     BrowserModule,
